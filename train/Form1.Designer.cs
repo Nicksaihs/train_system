15874744ca5deb29dd0c14b88cf0b7af52bb1e83
Nicksaihs <nickchang@saihs.edu.tw>
2025-03-18 16:57:06 +0800

    Add files via upload

---
+++ train/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace train
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button5 = new Button();
+            groupBox3 = new GroupBox();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            groupBox4 = new GroupBox();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 21);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 396);
+            textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(359, 27);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(406, 91);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "請按張數鍵";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(284, 33);
+            button4.Name = "button4";
+            button4.Size = new Size(84, 45);
+            button4.TabIndex = 3;
+            button4.Text = "4張";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(194, 33);
+            button3.Name = "button3";
+            button3.Size = new Size(84, 45);
+            button3.TabIndex = 2;
+            button3.Text = "3張";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(104, 33);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 45);
+            button2.TabIndex = 1;
+            button2.Text = "2張";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(14, 33);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 45);
+            button1.TabIndex = 0;
+            button1.Text = "1張";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button7);
+            groupBox2.Controls.Add(button8);
+            groupBox2.Location = new Point(359, 124);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(406, 91);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "請按車種鍵";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(194, 33);
+            button6.Name = "button6";
+            button6.Size = new Size(84, 45);
+            button6.TabIndex = 2;
+            button6.Text = "區間";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(104, 33);
+            button7.Name = "button7";
+            button7.Size = new Size(84, 45);
+            button7.TabIndex = 1;
+            button7.Text = "莒光";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(14, 33);
+            button8.Name = "button8";
+            button8.Size = new Size(84, 45);
+            button8.TabIndex = 0;
+            button8.Text = "自強";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(679, 221);
+            button5.Name = "button5";
+            button5.Size = new Size(86, 196);
+            button5.TabIndex = 5;
+            button5.Text = "出票";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button9);
+            groupBox3.Controls.Add(button10);
+            groupBox3.Controls.Add(button11);
+            groupBox3.Location = new Point(359, 221);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(305, 91);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "起始站";
+            // 
+            // button9
+            // 
+            button9.Location = new Point(194, 33);
+            button9.Name = "button9";
+            button9.Size = new Size(84, 45);
+            button9.TabIndex = 2;
+            button9.Text = "台南";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(104, 33);
+            button10.Name = "button10";
+            button10.Size = new Size(84, 45);
+            button10.TabIndex = 1;
+            button10.Text = "台中";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(14, 33);
+            button11.Name = "button11";
+            button11.Size = new Size(84, 45);
+            button11.TabIndex = 0;
+            button11.Text = "台北";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(button12);
+            groupBox4.Controls.Add(button13);
+            groupBox4.Controls.Add(button14);
+            groupBox4.Location = new Point(359, 326);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(305, 91);
+            groupBox4.TabIndex = 6;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "起始站";
+            // 
+            // button12
+            // 
+            button12.Location = new Point(194, 33);
+            button12.Name = "button12";
+            button12.Size = new Size(84, 45);
+            button12.TabIndex = 2;
+            button12.Text = "台南";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(104, 33);
+            button13.Name = "button13";
+            button13.Size = new Size(84, 45);
+            button13.TabIndex = 1;
+            button13.Text = "台中";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(14, 33);
+            button14.Name = "button14";
+            button14.Size = new Size(84, 45);
+            button14.TabIndex = 0;
+            button14.Text = "台北";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(button5);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button5;
+        private GroupBox groupBox3;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private GroupBox groupBox4;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+    }
+}
